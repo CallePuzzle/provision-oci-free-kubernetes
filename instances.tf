3dd504b9b5055cf9e630ef0458f2efd6baf48cf5
César M. Cristóbal <cesar@callepuzzle.com>
2023-08-13 13:00:16 +0200

    feat: add argocd apps and nginx

--- instances.tf
+++ instances.tf
@@ -72,7 +72,11 @@ module "instance" {
 resource "local_file" "k0sctl" {
   filename = var.k0s_config_path
   content = templatefile("${path.module}/k0sctl.yaml.tmpl", {
-    private_ip = module.instance["controllerworker"].private_ip[0]
-    public_ip = module.instance["controllerworker"].public_ip[0]
+    private_ip                 = module.instance["controllerworker"].private_ip[0]
+    public_ip                  = module.instance["controllerworker"].public_ip[0]
+    manifests_source           = var.manifests_source
+    enable_argocd              = var.enable_argocd
+    enable_argocd_apps         = var.enable_argocd_apps
+    enable_nginx_as_argocd_app = var.enable_nginx_as_argocd_app
   })
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "name" {
   description = "The name of the k0s cluster"
   type        = string
-  default    = "k0s"
+  default     = "k0s"
 }
 
 variable "compartment_id" {
@@ -12,4 +12,37 @@ variable "compartment_id" {
 variable "k0s_config_path" {
   description = "The path to the k0s config file"
   type        = string
+}
+
+variable "enable_argocd" {
+  description = "Enable ArgoCD"
+  type        = bool
+  default     = true
+}
+
+variable "enable_argocd_apps" {
+  description = "Enable ArgoCD apps"
+  type        = bool
+  default     = true
+}
+
+variable "enable_nginx_as_argocd_app" {
+  description = "Enable nginx as ArgoCD app"
+  type        = bool
+  default     = true
+}
+
+variable "manifests_source" {
+  description = "A yaml object with source variables for ArgoCD application manifests"
+  type = object({
+    repoURL        = string
+    targetRevision = string
+    path           = string
+    directory = optional(object({
+      recurse = optional(bool)
+      exclude = optional(string)
+      include = optional(string)
+    }))
+  })
+  default = null
 }
